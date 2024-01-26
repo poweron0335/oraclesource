55026393c17e587038ad1822bea70e7a6e4d7e2b
송민선 <poweron0335@gmail.com>
2024-01-26 18:11:10 +0900

    subquery,update,insert,delete

--- hr.sql
+++ hr.sql
@@ -433,7 +433,6 @@ WHERE d.DEPARTMENT_NAME LIKE 'Executive';
 	-- 각 사원별 소속 부서에서 자신보다 늦게 고용되었으나 많은 연봉을 받는 사원이 존재하는
 	-- 사원들의 이름 조회(이름은 first_name과 last_name을 결합하여 하나로 나오게 하기)
 	-- 부서번호, 결합된 이름, salary, hire_date 출력
-
 SELECT
 	DISTINCT e.department_id,
 	e.first_name || ' ' || e.last_name AS "내이름",
@@ -446,3 +445,341 @@ JOIN EMPLOYEES e2 ON
 	AND e.salary < e2.SALARY
 WHERE
 	e.HIRE_DATE < e2.HIRE_DATE;
+	
+-- 4
+-- LAST_NAME 에 u 가 포함되는 사원들과 동일 부서에서 근무하는 사원들의 사번 및 last_name 조회
+-- 사번 오름차순
+SELECT
+	EMPLOYEE_ID,
+	LAST_NAME
+FROM
+	EMPLOYEES e
+WHERE
+	DEPARTMENT_ID IN (
+	SELECT
+		DEPARTMENT_ID
+	FROM
+		EMPLOYEES e
+	WHERE
+		LAST_NAME LIKE '%u%')
+ORDER BY
+	EMPLOYEE_ID ASC;
+	
+-- 기존의 직업을 여전히 가지고 있는 사원들의 사번 및 job_id 조회
+SELECT
+	e.EMPLOYEE_ID, e.JOB_ID
+FROM
+	EMPLOYEES e
+WHERE (e.employee_id, e.job_id) IN (SELECT jh.employee_id, jh.job_id FROM JOB_HISTORY jh);
+
+
+-- JOIN 함수 사용
+SELECT DISTINCT 
+	e.employee_id,
+	e.job_id
+FROM
+	EMPLOYEES e
+JOIN JOB_HISTORY jh ON
+	e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE (e.EMPLOYEE_ID, e.JOB_ID) IN (SELECT jh.EMPLOYEE_ID, jh.JOB_ID FROM JOB_HISTORY jh);
+
+-- SELECT 서브쿼리
+-- 각 직무별 연봉 총합 및 각 부서별 연봉 총합 조회
+-- JOB_ID, 부서별연봉총합, 전체총합
+SELECT
+JOB_ID, (SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 10 AND E.job_id = E2.JOB_ID) AS DEPT10,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 20 AND E.job_id = E2.JOB_ID) AS DEPT20,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 30 AND E.job_id = E2.JOB_ID) AS DEPT30,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 40 AND E.job_id = E2.JOB_ID) AS DEPT40,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 50 AND E.job_id = E2.JOB_ID) AS DEPT50,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 60 AND E.job_id = E2.JOB_ID) AS DEPT60,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 70 AND E.job_id = E2.JOB_ID) AS DEPT70,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 80 AND E.job_id = E2.JOB_ID) AS DEPT80,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 90 AND E.job_id = E2.JOB_ID) AS DEPT90,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 100 AND E.job_id = E2.JOB_ID) AS DEPT100,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE department_id = 110 AND E.job_id = E2.JOB_ID) AS DEPT110,
+(SELECT SUM(SALARY) FROM EMPLOYEES e2 WHERE E.job_id = E2.JOB_ID) AS TOTAL
+FROM EMPLOYEES e
+GROUP BY JOB_ID
+ORDER BY JOB_ID;
+
+
+-- 예시문제 --
+-- JOB_ID 가 SA_MAN 인 사원들의 최대 연봉보다 높게 받는 사원들의 LAST_NAME, JOB_ID, SALARY 조회
+SELECT last_name, job_id, SALARY
+FROM EMPLOYEES e
+WHERE SALARY < ANY (SELECT Max(SALARY) FROM EMPLOYEES e WHERE JOB_ID = 'SA_MAN');
+/*오답노트*/
+SELECT
+	last_name,
+	job_id,
+	salary
+FROM
+	EMPLOYEES e
+WHERE
+	SALARY > (
+	SELECT
+		Max(SALARY)
+	FROM
+		EMPLOYEES e
+	WHERE
+		JOB_ID = 'SA_MAN'); 
+
+-- 커미션을 버는 사원들의 부서와 연봉이 동일한 사원들의 LAST_NAME, 부서번호, 연봉 조회
+/*내가 한 문제풀이*/
+SELECT
+	Last_name,
+	DEPARTMENT_ID,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	COMMISSION_PCT = ANY (
+	SELECT
+		COMMISSION_PCT
+	FROM
+		EMPLOYEES e);
+/*오답노트*/
+SELECT
+	last_name,
+	department_id,
+	salary
+FROM
+	EMPLOYEES e
+where
+(DEPARTMENT_ID,
+	SALARY) IN (
+	SELECT
+	DEPARTMENT_ID, SALARY
+	FROM
+		EMPLOYEES e
+	WHERE
+		COMMISSION_PCT IS NOT NULL); 
+
+-- 회사 전체 평균 연봉보다 더 많이 버는 사원들 중 LAST_NAME에 u가 있는 사원들이 근무하는 부서에서 근무하는
+-- 사원들의 사번, LAST_NAME, 연봉 조회
+/*내가 한 문제풀이*/
+SELECT
+	EMPLOYEE_ID,
+	LAST_NAME,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	SALARY > (
+	SELECT
+		avg(SALARY)
+		FROM EMPLOYEES e)
+	AND LAST_NAME LIKE '%u%';
+/*오답노트*/
+SELECT
+	EMPLOYEE_ID,
+	LAST_NAME,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	department_id IN
+(
+	SELECT
+		DEPARTMENT_ID
+	FROM
+		EMPLOYEES e
+	WHERE
+		salary > (
+		SELECT
+			avg(SALARY)
+		FROM
+			EMPLOYEES e)
+		AND last_name LIKE '%u%'); 
+-- 각 부서별 평균 연봉보다 더 받는 동일 부서 근무사원들의 LAST_NAME, 부서번호, 연봉 및 해당 부서의 평균 연봉 조회
+-- 부서별 연봉을 기준으로 정렬
+/*내가 한 문제풀이*/
+SELECT
+	LAST_NAME,
+	DEPARTMENT_ID,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+    SALARY < (SELECT avg(SALARY) FROM EMPLOYEES e)
+  ORDER BY DEPARTMENT_ID;
+ /*오답노트*/
+SELECT
+E1.last_name, E1.DEPARTMENT_ID, E1.SALARY, E2.DEPT_SAL_AVG
+FROM
+	EMPLOYEES E1,
+	(
+	SELECT
+		DEPARTMENT_ID,
+		avg(SALARY) AS DEPT_SAL_AVG
+	FROM
+		EMPLOYEES e
+	GROUP BY
+		DEPARTMENT_ID) E2
+WHERE             
+	E1.department_ID = E2.department_ID AND E1.Salary > E2.DEPT_SAL_AVG 
+ORDER BY DEPARTMENT_ID
+;
+
+-- 
+
+-- LAST_NAME이 Davies 인 사람보다 후에 고용된 사원들의 last_name, hire_date 조회
+SELECT
+	LAST_NAME,
+	HIRE_DATE
+FROM
+	EMPLOYEES e
+WHERE
+	HIRE_DATE > (
+	SELECT
+		HIRE_DATE
+	FROM
+		EMPLOYEES e
+	WHERE
+		LAst_name LIKE 'Davies');
+
+-- last_name 이 king 인 사원을 매니저로 두고 있는 모든 사원들의 last_name, salary 조회
+/*내가 한 문제풀이*/
+SELECT
+	last_name,
+	SALARY,
+	MANAGER_ID
+FROM
+	EMPLOYEES e
+WHERE
+	(MANAGER_ID,
+	DEPARTMENT_ID) IN (
+	SELECT
+		MANAGER_ID,
+		DEPARTMENT_ID
+	FROM
+		EMPLOYEES e
+	WHERE
+		last_name = 'King');
+ /*오답노트*/
+SELECT
+	LAST_NAME,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	manager_ID IN (
+	SELECT
+		EMPLOYEE_ID
+	FROM
+		EMPLOYEES e
+	WHERE
+		LAST_NAME = 'King');
+-- last_name 이 Kochhar 인 사원과 동일한 연봉 및 커미션을 버는 사원들의 last_name, 부서번호, salary 조회
+-- Kochhar은 제외한다
+SELECT
+	last_name,
+	DEPARTMENT_ID,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	(salary,
+	nvl(commission_pct, 0)) IN (
+	SELECT
+		salary,
+		nvl(commission_pct, 0)
+	FROM
+		EMPLOYEES e
+	WHERE
+		last_name = 'Kochhar') AND e.LAST_NAME != 'Kochhar';
+	
+-- 부서가 위치한 국가 ID, 국가 이름 조회
+-- countries 테이블과 loactions, departments 이용
+SELECT
+	COUNTRY_ID,
+	COUNTRY_NAME
+FROM
+	COUNTRIES c
+WHERE
+	country_ID IN (
+	SELECT
+		l.COUNTRY_ID
+	FROM
+		LOCATIONS l ,
+		(
+		SELECT
+			LOCATION_ID
+		FROM
+			DEPARTMENTS d) D
+	WHERE
+		l.LOCATION_ID = D.location_ID)
+	
+-- 총 사원 수 및 2013,2014,2015,2016 년도 별 고용된 사원들의 총 수를 조회한다.
+
+--SELECT COUNT(EMPLOYEE_ID) FROM EMPLOYEES e WHERE to_char(HIRE_DATE, 'YYYY') = '13';
+--SELECT COUNT(EMPLOYEE_ID) FROM EMPLOYEES e WHERE HIRE_DATE LIKE '13%'	
+
+
+SELECT DISTINCT
+    (SELECT
+		count(EMPLOYEE_ID) FROM EMPLOYEES e2) AS "총 사원수" ,
+	(
+	SELECT
+		COUNT(EMPLOYEE_ID)
+	FROM
+		EMPLOYEES e
+	WHERE
+		to_char(HIRE_DATE, 'YY') = '13'
+	) AS "2013",
+	(
+	SELECT
+		COUNT(EMPLOYEE_ID)
+	FROM
+		EMPLOYEES e
+	WHERE
+		to_char(HIRE_DATE, 'YY') = '14'
+	) AS "2014",
+	(
+	SELECT
+		COUNT(EMPLOYEE_ID)
+	FROM
+		EMPLOYEES e
+	WHERE
+		to_char(HIRE_DATE, 'YY') = '15'
+	) AS "2015",
+	(
+	SELECT
+		COUNT(EMPLOYEE_ID)
+	FROM
+		EMPLOYEES e
+	WHERE
+		to_char(HIRE_DATE, 'YY') = '16'
+	) AS "2016"
+FROM
+	EMPLOYEES e;
+
+-- 위치 ID 가 1700인 사원들의 연봉과 커미션을 추출한 뒤, 추출된 사원들의 연봉과 커미션이
+-- 동일한 사원정보 조회
+-- 사원번호, 이름( frist+last 결합), 부서번호, 급여 출력
+
+SELECT
+	EMPLOYEE_ID,
+	FIRST_NAME || ' ' || LAST_NAME AS NAME,
+	DEPARTMENT_ID,
+	SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	(salary,
+	nvl(COMMISSION_PCT, 0)) IN (
+	SELECT
+		SALARY,
+		nvl(COMMISSION_PCT, 0)
+	FROM
+		EMPLOYEES e
+	WHERE
+		department_ID IN (
+		SELECT
+			DEPARTMENT_ID
+		FROM
+			DEPARTMENTS d
+		WHERE
+			D.LOCATION_ID = 1700));
+			
--- scott.sql
+++ scott.sql
@@ -969,3 +969,388 @@ LEFT OUTER JOIN emp e ON
 ORDER BY
 	d.deptno,
 	e.ename;
+
+-- 4일차
+
+-- 서브쿼리 --
+-- SQL 문 안에 내부에서 SELECT 문을 사용
+-- 괄호로 묶어서 사용
+-- 메인쿼리의 비교 대상과 같은 자료형과 같은 개수로 지정해야 한다.
+
+-- JONES 사원의 급여보다 높은 급여를 받는 사원 조회
+-- JONES 급여 알아내기 => 비교
+SELECT SAL  
+FROM EMP e
+WHERE ENAME = 'JONES';
+SELECT * FROM EMP e WHERE SAL > 2975;
+
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	SAL > (
+	SELECT
+		SAL
+	FROM
+		EMP e
+	WHERE
+		ENAME = 'JONES');
+		
+-- ALLEN 이 받는 COMM 보다 많은 추가수당을 받는 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	COMM > (
+	SELECT
+	COMM 
+	FROM
+		EMP e
+	WHERE
+		ename = 'ALLEN');
+
+--	WARD 사원의 입사일보다 빠른 입사자 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	HIREDATE < (
+	SELECT
+		HIREDATE
+	FROM
+		EMP e
+	WHERE
+		ENAME = 'WARD');
+
+-- 20번 부서에 속한 사원 중 전체 사원의 평균 급여보다 높은 급여를 받는 사원 조회
+-- 사원번호, 사원명, 직무, 급여, 부서번호, 부서명, 지역
+
+SELECT
+	e.EMPNO,
+	e.ENAME,
+	e.JOB,
+	e.SAL,
+	d.DEPTNO,
+	d.DNAME,
+	d.LOC
+FROM
+	EMP e
+JOIN DEPT d ON
+	e.DEPTNO = d.DEPTNO
+WHERE
+	e.DEPTNO = 20
+	AND e.SAL > (
+	SELECT
+		avg(sal)
+	FROM
+		EMP e);
+		
+-- 단일행 서브쿼리 : 서브쿼리 실행 결과가 단 하나의 행으로 나오는 서브쿼리
+--   사용가능한 연산자 : >, <, >=, <=, <>, ^=, !=
+
+-- 다중행 서브쿼리 : 서브쿼리 실행 결과가 여러개의 행으로 나오는 서브쿼리
+--   사용가능한 연산자 : IN, ANY(SOME), ALL, EXISTS	
+--                       IN, ANY(SOME) : 메인 쿼리의 조건식을 만족하는 서브쿼리가 하나이상
+--                       ALL(메인쿼리의 조건식을 서브쿼리의 결과 모두가 만족)
+--                       EXISTS(서브 쿼리의 결과가 존재하면)
+
+-- 단일 행 하위 질의에 2개 이상의 행이 리턴되었습니다.
+-- 서브쿼리가 여러 개의 결과값을 리턴하는데 단일행 서브쿼리에 사용하는 연산자가 사용된 경우
+--SELECT * FROM EMP e WHERE SAL >= (SELECT MAX(sal) FROM	EMP e GROUP BY DEPTNO);
+	
+SELECT * FROM EMP e WHERE SAL IN (SELECT MAX(sal) FROM	EMP e GROUP BY DEPTNO);
+
+-- IN 사용과 동일한 결과
+SELECT * FROM EMP e WHERE SAL = ANY (SELECT MAX(sal) FROM	EMP e GROUP BY DEPTNO);
+
+-- 30번 부서 사원들의 최대 급여보다 적은 급여를 받는 사원 조회
+SELECT * FROM EMP e WHERE SAL < ANY (SELECT SAL FROM eMP e WHERE DEPTNO = 30);
+
+-- 서브쿼리 결과값이 모두 만족하면 나온다.
+SELECT * FROM EMP e WHERE SAL < ALL (SELECT SAL FROM eMP e WHERE DEPTNO = 30);
+
+-- 서브쿼리 결과값이 하나라도 만족하면 모두 나온다.
+SELECT * FROM EMP e WHERE EXISTS (SELECT DNAME FROM DEPT  WHERE DEPTNO = 20);
+
+-- 전체 사원 중 ALLEN과 같은 직책인 사원들의 사원정보, 부서 정보를 다음과 같이 출력하는 SQL 문을 작성하시오.
+SELECT
+	e.JOB,
+	e.EMPNO,
+	e.ENAME,
+	e.SAL,
+	e.DEPTNO,
+	d.DNAME
+FROM
+	EMP e
+JOIN DEPT d ON
+	e.DEPTNO = d.DEPTNO
+WHERE
+	e.JOB IN (
+	SELECT
+		e.JOB
+	FROM
+		EMP e
+	WHERE
+		e.ENAME = 'ALLEN')
+;
+
+-- 전체 사원의 평균 급여보다 높은 급여를 받는 사원들의 사원정보, 부서정보, 급여 등급 정보를 출력하는 SQL문을 작성하시오
+-- 단, 출력할 때 급여가많은 순으로 정렬하되 급여가 같은 경우에는 사원 번호를 기준으로 오름차순으로 정렬하기
+SELECT
+	e.EMPNO,
+	e.ENAME,
+	d.DNAME,
+	e.HIREDATE,
+	d.LOC,
+	e.SAL,
+	s.GRADE
+FROM
+	EMP e
+JOIN DEPT d ON
+	e.DEPTNO = d.DEPTNO
+JOIN SALGRADE s ON
+	e.sal BETWEEN s.LOSAL AND s.HISAL
+WHERE
+	e.SAL > (
+	SELECT
+		AVG(e.SAL)
+	FROM
+		emp e)
+ORDER BY
+	e.sal DESC,
+	e.EMPNO ASC;
+	
+-- 다중 열 서브쿼리
+-- 부서별 급여 최대값 
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	(DEPTNO,
+	SAL) IN 
+(
+	SELECT
+		DEPTNO,
+		MAX(SAL)
+	FROM
+		EMP e
+	GROUP BY
+		DEPTNO);
+		
+--  FROM 절에 사용하는 서브쿼리(인라인 뷰)
+-- JOIN 함수도 가능
+SELECT
+	e10.empno,
+	e10.ename,
+	e10.deptno,
+	D.dname,
+	D.loc
+FROM
+	(
+	SELECT
+		*
+	FROM
+		emp
+	WHERE
+		deptno = 10) e10,
+	 (
+	SELECT
+		*
+	FROM
+		DEPT) D
+WHERE
+	E10.deptno = D.deptno;
+	
+-- SELECT 절에 사용하는 서브쿼리(스칼라 서브쿼리)
+SELECT
+	Empno,
+	ename,
+	job,
+	sal,
+	(
+	SELECT
+		Grade
+	FROM
+		SALGRADE s
+	WHERE
+		e.sal BETWEEN s.losal AND s.hisal) AS SALGRADE
+FROM
+	EMP e;
+
+-- DML(Data Manipulation Language-데이터 조작 언어)
+/* SELECT(조회), INSERT(삽입), UPDATE(수정), DELETE(삭제) */
+
+
+-- 기존 테이블 복제해서 새로운 테이블 생성
+CREATE TABLE DEPT_TEMP AS SELECT * FROM DEPT;
+
+-- 새로운 부서 추가
+-- INSERT INTO 테이블명(열이름1, 열이름2, ....)
+-- VALUES(데이터1, 데이터2, .....)
+
+INSERT INTO DEPT_TEMP(DEPTNO, DNAME, LOC)
+VALUES(60, 'DATABESE', 'BUSAN');
+
+-- 열이름은 필수가 아니긴 함
+INSERT INTO DEPT_TEMP
+VALUES(70, 'DATABESE', 'BUSAN');
+
+-- 값의 수가 충분하지 않습니다.
+INSERT INTO DEPT_TEMP
+VALUES(80, 'DATABESE');
+
+-- 개수를 안 맞춰서도 넣을 수 있지만 개수를 맞춰줘야하고 안 넣은 값은 NULL로 나온다.
+INSERT INTO DEPT_TEMP(DEPTNO, DNAME)
+VALUES(80, 'DATABESE');
+
+-- 이 열에 대해 지정된 전체 자릿수보다 큰 값이 허용됩니다.
+INSERT INTO DEPT_TEMP(DEPTNO, DNAME)
+VALUES(800, 'DATABESE');
+
+INSERT INTO DEPT_TEMP(DEPTNO, DNAME, LOC)
+VALUES(90, 'DATABESE', NULL);
+
+
+CREATE TABLE EMP_TEMP AS SELECT * FROM EMP;
+
+
+INSERT INTO EMP_TEMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(8000, 'HONG', 'MANAGER', 7902, '2015-03-15', 1000, NULL, 50);
+
+INSERT INTO EMP_TEMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7500, 'HONG', 'MANAGER', 7902, '2015-03-15', 1000, NULL, 50);
+
+INSERT INTO EMP_TEMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(9000, 'SUNG', 'MANAGER', 7782, SYSDATE, 1200, 800, 50);
+
+INSERT INTO EMP_TEMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7000, 'SUNG', 'MANAGER', 7782, SYSDATE, 1200, 800, 50);
+
+-- 테이블의 구조만 복사(데이터는 복사하지 않을 때)
+CREATE TABLE EMP_TEMP2 AS SELECT * FROM EMP WHERE 1<>1;
+
+-- 수정
+-- UPDATE 테이블명 SET 수정할 내용,.....,.....;             ①
+-- UPDATE 테이블명 SET 수정할 내용,.....,..... where 조건;  ②
+
+UPDATE
+	DEPT_TEMP
+SET
+	loc = 'BUSAN';
+	
+UPDATE
+	DEPT_TEMP
+SET
+	loc = 'SEOUL'
+WHERE DEPTNO = 50;
+
+UPDATE
+	DEPT_TEMP
+SET
+	loc = 'SEOUL', DNAME = 'NETWORK'
+WHERE DEPTNO = 40;
+
+
+
+-- 삭제(행단위)
+-- DELETE 테이블명 WHERE 조건 
+-- DELETE * FROM 테이블명 WHERE 조건 
+
+DELETE dept_TEMP WHERE deptno = 20;
+
+DELETE FROM DEPT_TEMP 
+WHERE deptno = 30;
+
+-- 서브쿼리 + DELETE
+-- 급여등급이 3등급이고 30번 부서의 사원 삭제
+
+DELETE
+FROM
+	EMP_TEMP
+WHERE
+	empno IN (
+	SELECT
+		empno
+	FROM
+		EMP_TEMP et
+	JOIN SALGRADE s ON
+		et.sal BETWEEN s.losal AND s.hisal
+		AND s.grade = 3
+		AND et.deptno = 30);
+		
+-- 서브쿼리 + UPDATE
+	
+UPDATE
+	DEPT_TEMP
+SET
+	(DNAME,
+	LOC) = (
+	SELECT
+		DNAME,
+		LOC
+	FROM
+		DEPT
+	WHERE
+		DEPTNO = 40)
+WHERE DEPTNO = 40;
+
+-- 서브쿼리 + INSERT
+
+INSERT
+	INTO
+	EMP_TEMP(EMPNO,
+	ENAME,
+	JOB,
+	MGR,
+	HIREDATE,
+	SAL,
+	COMM,
+	DEPTNO)
+SELECT
+	E1.EMPNO,
+	E1.ENAME,
+	e1.JOB,
+	e1.MGR,
+	e1.HIREDATE,
+	e1.SAL,
+	e1.comm,
+	e1.DEPTNO
+FROM
+	EMP E1
+JOIN SALGRADE s ON
+	e1.SAL BETWEEN S.LOSAL AND s.HISAL
+	AND S.grade = 1;
+	
+
+CREATE TABLE EXAM_EMP AS SELECT * FROM EMP WHERE 1<>1;
+
+CREATE TABLE EXAM_DEPT AS SELECT * FROM DEPT WHERE 1<>1;
+
+CREATE TABLE EXAM_SALGRADE AS SELECT * FROM SALGRADE WHERE 1<>1;
+
+
+
+CREATE TABLE EXAM_EMP AS SELECT * FROM EMP WHERE 1<>1;
+
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7201, 'TEST_USER1', 'MANAGER', 7788, '2016-01-02', 4500, '', 50);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7202, 'TEST_USER2', 'CLERK', 7201, '2016-02-21', 1800, '', 50);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7203, 'TEST_USER3', 'MANAGER', 7201, '2016-04-11', 3400, '', 60);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7204, 'TEST_USER4', 'SALESMAN', 7201, '2016-05-31', 2700, 300, 60);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7205, 'TEST_USER5', 'CLERK', 7201, '2016-07-20', 2600, '', 70);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7206, 'TEST_USER6', 'CLERK', 7201, '2016-09-08', 2600, '', 70);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7207, 'TEST_USER7', 'LECTURER', 7201, '2016-10-28', 2300, '', 80);
+INSERT INTO EXAM_EMP(EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+VALUES(7208, 'TEST_USER8', 'STUDENT', 7201, '2018-03-09', 1200, '', 80);
+COMMIT
+
